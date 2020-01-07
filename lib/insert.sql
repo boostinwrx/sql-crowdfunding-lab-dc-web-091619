72c1627dcc0b9b6620abc9a5b63dc1391c17851a
boostinwrx <tony.calablava@gmail.com>
2020-01-07 16:47:13 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,50 @@
+
+INSERT INTO users (id, name, age) VALUES (1, 'Tiff', 17), (2, 'Anna', 18), (3, 'Paul', 14), (4, 'Annie', 3),
+(5, 'Redundancy', 2), (6, 'Redundancy', 2), (7, 'Redundancy', 34), (8, 'Redundancy', 24), (9, 'Redundancy', 130), (10, 'Redundancy', 310),
+(11, 'Redundancy', 900), (12, 'Redundancy', 21111), (13, 'Redundancy', 400), (14, 'Redundancy', 900), (15, 'Redundancy II', 1139), (16, 'Redundancy II', 15),
+(17, 'Redundancy II', 86), (18, 'Redundancy II', 5), (19, 'Redundancy II', 422), (20, 'Redundancy II', 233);
+
+
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES
+(1, 'ACLU', 'civil rights', 1000.00, '2019-01-30', '2019-07-30'),
+(2, 'Planned Parenthood', 'womens rights', 2000.00, '2019-01-01', '2019-01-01'),
+(3, 'I already finished this module', 'fact', 1000.00, '2019-05-30', '2015-05-30'),
+(4, 'I already finished this module', 'fact', 400.00, '2019-03-14', '2019-01-30'),
+(5, 'I already finished this module', 'fact', 6000.00, '2019-03-20', '2019-09-20'),
+(6, 'I already finished this module', 'fact', 200.00, '2019-12-30', '2015-12-30'),
+(7, 'I already finished this module', 'fact', 2000.00, '2019-10-02', '2019-10-30'),
+(8, 'I already finished this module!', 'fact', 800.00, '2019-01-30', '2019-01-31'),
+(9, 'I already finished this module!!', 'fact', 3000.00, '2019-01-30', '2019-09-30'),
+(10, 'I already finished this module!!!!', 'fact', 5000.00, '2012-03-20', '2019-01-30');
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES
+(1, 10.00, 1, 2),
+(2, 20.00, 1, 3),
+(3, 40.00, 1, 4),
+(4, 50.00, 2, 3),
+(5, 10.00, 3, 2),
+(6, 20.00, 4, 4),
+(7, 40.00, 5, 10),
+(8, 60.00, 6, 10),
+(9, 50.00, 7, 9),
+(10, 700.00, 8, 8),
+(11, 1000.00, 8, 7),
+(12, 40.00, 9, 6),
+(13, 50.00, 9, 3),
+(14, 50.00, 10, 4),
+(15, 24.00, 12, 1),
+(16, 34.00, 11, 1),
+(17, 12.00, 13, 6),
+(18, 19.00, 14, 5),
+(19, 20.00, 15, 5),
+(20, 40.00, 16, 6),
+(21, 35.50, 17, 7),
+(22, 40.00, 18, 8),
+(23, 60.00, 19, 9),
+(24, 70.00, 20, 10),
+(25, 100.00, 20, 4),
+(26, 40.00, 19, 1),
+(27, 20.00, 18, 6),
+(28, 90.00, 17, 9),
+(29, 230.00, 16, 6),
+(30, 450.00, 15, 5);
